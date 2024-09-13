f3cf81f96c6b5b0e793f18c70ca2a52b58b5ae18
Mikhael Gaster <mikhaelgaster@gmail.com>
2024-09-13 16:54:26 -0700

    packaging

--- src/record_consolidation/__init__.py
+++ src/record_consolidation/__init__.py
@@ -1,2 +1,4 @@
-def hello() -> str:
-    return "Hello from record-consolidation!"
+from .df_consolidations import consolidate_normalized_table
+
+__all__ = ["consolidate_normalized_table"]
+__version__ = "0.1.0"
