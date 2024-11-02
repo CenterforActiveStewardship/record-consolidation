56b4410bcc92d6b1adebfbb725a8845df115ba1f
Mikhael Gaster <mikhaelgaster@gmail.com>
2024-11-01 18:27:39 -0700

    minor: version

--- src/record_consolidation/__init__.py
+++ src/record_consolidation/__init__.py
@@ -2,4 +2,4 @@ from .df_consolidations import normalize_subset
 from .graphs import atomize_records
 
 __all__ = ["normalize_subset", "atomize_records"]
-__version__ = "0.4.0"
+__version__ = "0.4.1"
